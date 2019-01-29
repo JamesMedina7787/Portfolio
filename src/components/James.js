4bb72a702aa9de18d93efee62fd06b592c63dcbf
james medina <jamesmedina7787@outlook.com>
2019-01-29 18:19:26 -0500

    heroku ready

---
+++ src/components/James.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class James extends Component {
+  render() {
+    return (
+      <div className="James">
+James Wilfred Medina
+      </div>
+    );
+  }
+}
+
+export default James;
